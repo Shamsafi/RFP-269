542be8a9bd226a14b546081f8f35947f6d34b95c
Shamsafi <118178673+Shamsafi@users.noreply.github.com>
2023-03-23 08:39:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Use_case1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            UC6.GetAttendance();
+        }
+    }
+}
